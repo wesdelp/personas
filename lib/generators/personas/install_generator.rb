0b3e70b4149f9a13a1c1368270757ad50a928594
Braden Polly <bpolly@covermymeds.com>
2019-06-29 21:52:21 -0400

    Inject module include in routes file if needed

--- lib/generators/personas/install_generator.rb
+++ lib/generators/personas/install_generator.rb
@@ -14,6 +14,33 @@ module Personas
       def create_persona_model_directory
         empty_directory 'app/models/persona'
       end
+
+      def inject_include_personas_router
+        return unless needs_router_module?
+
+        insert_into_file 'config/routes.rb',
+                         include_router_block,
+                         before: "Rails.application.routes.draw do\n"
+      end
+
+      private
+
+      def needs_router_module?
+        File.readlines('config/routes.rb').none? do |line|
+          line =~ /include #{persona_router_module}/
+        end
+      end
+
+      def include_router_block
+        <<~RUBY
+          include #{persona_router_module}
+
+        RUBY
+      end
+
+      def persona_router_module
+        'Personas::PersonaRouter'
+      end
     end
   end
 end
